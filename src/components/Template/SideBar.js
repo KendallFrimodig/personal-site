f239fc45c3e66af4742587983a631939f46b5d62
kendallfrimodig <kendallfrimodig@gmail.com>
2021-04-11 16:07:34 -0600

    initial adaptations

--- src/components/Template/SideBar.js
+++ src/components/Template/SideBar.js
@@ -12,21 +12,21 @@ const SideBar = () => (
         <img src={`${PUBLIC_URL}/images/me.jpg`} alt="" />
       </Link>
       <header>
-        <h2>Michael D&apos;Angelo</h2>
-        <p><a href="mailto:michael.l.dangelo@gmail.com">michael.l.dangelo@gmail.com</a></p>
+        <h2>Kendall Frimodig</h2>
+        <p><a href="mailto:kendallfrimodig@gmail.com">kendallfrimodig@gmail.com</a></p>
       </header>
     </section>
 
     <section className="blurb">
       <h2>About</h2>
-      <p>Hi, I&apos;m Michael. I like building things.
-        I am a <a href="https://icme.stanford.edu/">Stanford ICME</a> graduate, YC Alumni, and
-        the co-founder and CTO of <a href="https://arthena.com">Arthena</a>. Before Arthena I was
-        at <a href="https://matroid.com">Matroid</a>
-        , <a href="https://planet.com">Planet</a>
-        , <a href="https://planetaryresources.com">Planetary Resources</a>
-        , <a href="https://facebook.com">Facebook</a>
-        , and <a href="https://seds.org">SEDS</a>.
+      <p>minimalist, epidemiologist, musician, outdoorsman & michigander in my third
+        year of a career in analytics. I am a MPH graduate, GVSU alumni, and
+        currently the environmental tracking epidemiologist
+        at <a href="https://www.nmhealth.org/">new mexico department of health</a>. previously I was
+        at <a href="https://www.tpchd.org/">tacoma-pierce county health department</a>
+        , and provided statistical and GIS support for
+        <a href="https://desmondtutuhealthfoundation.org.za/">the desmond tutu HIV foundation</a>
+        and <a href="https://www.accesskent.com/Health/">kent county health department</a>.
       </p>
       <ul className="actions">
         <li>
@@ -37,7 +37,7 @@ const SideBar = () => (
 
     <section id="footer">
       <ContactIcons />
-      <p className="copyright">&copy; Michael D&apos;Angelo <Link to="/">mldangelo.com</Link>.</p>
+      <p className="copyright">&copy; Kendall Frimodig <Link to="/">kendallfrimodig.com</Link>.</p>
     </section>
   </section>
 );
--- src/pages/Index.js
+++ src/pages/Index.js
@@ -13,8 +13,7 @@ const Index = () => (
         <div className="title">
           <h2 data-testid="heading"><Link to="/">About this site</Link></h2>
           <p>
-            A beautiful, responsive, statically-generated,
-            react application written with modern Javascript.
+            A collection of vignettes aimed to convey a sense of my character and utility
           </p>
         </div>
       </header>
@@ -25,7 +24,6 @@ const Index = () => (
         view <Link to="/stats">site statistics</Link>, {' '}
         or <Link to="/contact">contact</Link> me.
       </p>
-      <p> Source available <a href="https://github.com/mldangelo/personal-site">here</a>.</p>
     </article>
   </Main>
 );
